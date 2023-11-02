ebec4aac4a4a3a70c3b92e2d3f58a340396ca9c3
Vishaq Jayakumar <vishaqj@Vishaqs-Laptop.local>
2023-11-02 13:21:23 -0400

    Changes

---
+++ src/Labs/a4/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Add from "./Add";
+import ClickEvent from "./ClickEvent";
+
+const Assignment4 = () => {
+  return (
+    <>
+      <h1>Assignment 4</h1>
+      <Add a={1} b={2} />
+      <ClickEvent/>
+    </>
+  );
+};
+export default Assignment4;
+
