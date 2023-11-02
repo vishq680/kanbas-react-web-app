db46219660d980ac14909425e5adbd846de6f87a
Vishaq Jayakumar <vishaqj@Vishaqs-Laptop.local>
2023-11-02 13:31:27 -0400

    Nerw changes

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Add from "./Add";
 import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 
 const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <>
       <h1>Assignment 4</h1>
       <Add a={1} b={2} />
       <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
     </>
   );
 };
