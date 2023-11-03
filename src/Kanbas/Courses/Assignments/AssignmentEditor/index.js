1c9cf1999c23fc6d5d129e556001b936ae5b98f9
Vishaq Jayakumar <vishaqj@Vishaqs-Laptop.local>
2023-11-02 23:37:02 -0400

    Assignment complete

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -1,29 +1,90 @@
 import React from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import db from "../../../Database";
+import { deleteAssignment, addAssignment, selectAssignment, updateAssignment } from "../assignmentsReducer";
+import { useSelector, useDispatch } from "react-redux";
 
 
 function AssignmentEditor() {
   const { assignmentId } = useParams();
-  const assignment = db.assignments.find(
-    (assignment) => assignment._id === assignmentId);
+  // const assignment = db.assignments.find(
+  //   (assignment) => assignment._id === assignmentId);
 
 
-  const { courseId } = useParams();
+  const { courseId } = useParams(); 
+  const assignments = useSelector((state) => state.assignmentsReducer.assignments);
+  const assignment = useSelector((state) => state.assignmentsReducer.assignment);
+  console.log("first time: "+ JSON.stringify(assignment))
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleSave = () => {
     console.log("Actually saving assignment TBD in later assignments");
+
+    console.log("hahdsja: "+ JSON.stringify(assignment))
+
+    if(assignment._id){
+      dispatch(updateAssignment(assignment))
+    }
+    else {
+      dispatch(addAssignment({...assignment, course: courseId}))
+    }
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
   return (
     <div>
-      <h2>Assignment Name</h2>
-      <input value={assignment.title}
-             className="form-control mb-2" />
+      <div className="form-group">
+        <h2>Assignment Name</h2>
+        <input value={assignment.title}
+          className="form-control mb-2"
+          onChange={(e) =>
+            dispatch(selectAssignment({ ...assignment, title: e.target.value }))
+          } />
+        <textarea
+          className="form-control"
+          value={assignment.description}
+          onChange={(e) =>
+            dispatch(selectAssignment({ ...assignment, description: e.target.value }))
+          } />
+        <label>Points</label>
+        <input class="form-control"
+          placeholder="Enter points"
+          onChange={(e) =>
+            dispatch(selectAssignment({ ...assignment, points: e.target.value }))}
+        />
+        <label>Assignment Due</label>
+        <input class="form-control"
+          type="date"
+          onChange={(e) =>
+            dispatch(selectAssignment({ ...assignment, dueDate: e.target.value }))}
+        />
+        <form>
+          <div class="row">
+            <div class="col">
+              <label>Available From</label>
+              <input class="form-control"
+                type="date"
+                onChange={(e) =>
+                  dispatch(selectAssignment({ ...assignment, fromDate: e.target.value }))}
+              />
+            </div>
+            <div class="col">
+              <label>Until</label>
+              <input class="form-control"
+                type="date"
+                onChange={(e) =>
+                  dispatch(selectAssignment({ ...assignment, untilDate: e.target.value }))}
+              />
+            </div>
+          </div>
+        </form>
+
+
+      </div>
+
       <Link to={`/Kanbas/Courses/${courseId}/Assignments`}
-            className="btn btn-danger">
+        className="btn btn-danger">
         Cancel
-      </Link>   
+      </Link>
       <button onClick={handleSave} className="btn btn-success me-2">
         Save
       </button>
--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -2,20 +2,35 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
 import "./index.css"
+import { deleteAssignment, addAssignment, selectAssignment, updateAssignment } from "./assignmentsReducer";
+import { useSelector, useDispatch } from "react-redux";
 
+// const assignments = useSelector((state) => state.assignmentsReducer.modules);
+// const assignment = useSelector((state) => state.assignmentsReducer.module);
+// const dispatch = useDispatch();
 
 function Assignments() {
     const { courseId } = useParams();
-    const assignments = db.assignments;
-    const courseAssignments = assignments.filter(
-        (assignment) => assignment.course === courseId);
+    // const assignments = db.assignments;
+    // const courseAssignments = assignments.filter(
+    //     (assignment) => assignment.course === courseId);
+    const assignments = useSelector((state) => state.assignmentsReducer.assignments);
+    const assignment = useSelector((state) => state.assignmentsReducer.assignment);
+    const dispatch = useDispatch();
+
     return (
         <div>
             <div>
                 <input type="name" class="btn" placeholder="Search Assignments" />
-                <button style={{float: "right"}} type="button" class="btn btn-light">Group</button>
-                <button style={{float: "right"}} type="button" class="btn btn-light">Assignment</button>
-                <select style={{float: "right"}} class="btn">
+                <button style={{ float: "right" }} type="button" class="btn btn-light">Group</button>
+                {/* <button style={{ float: "right" }} type="button" class="btn btn-success">Assignment +</button> */}
+                <Link
+                    key={assignment._id}
+                    to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                    className="no-underline">
+                    <button style={{ float: "right" }} type="button" class="btn btn-success" onClick={() => dispatch(selectAssignment({ title: "New Assignment", course: "New Course", dueDate:"", fromDate:"", untilDate:"" }))}>Assignment +</button>
+                </Link>
+                <select style={{ float: "right" }} class="btn">
                     <option selected value="EAD">
                         Edit Assignment dates</option>
                 </select>
@@ -25,14 +40,55 @@ function Assignments() {
             <hr /><hr />
             <h2>Assignments for course {courseId}</h2>
             <div className="list-group">
-                {courseAssignments.map((assignment) => (
+                {/* {courseAssignments.map((assignment) => (
                     <Link
                         key={assignment._id}
                         to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
                         className="list-group-item">
                         {assignment.title}
+                        <button
+                            type="button"
+                            class="btn btn-danger"
+                            onClick={() => dispatch(deleteAssignment(assignment._id))}>
+                            Delete
+                        </button>
                     </Link>
-                ))}
+                ))} */}
+                {
+                    assignments
+                        .filter((assignment1) => assignment1.course === courseId)
+                        .map((assign, index) => (
+
+                            <li key={index} className="list-group-item">
+                                <div>
+                                        <h4>{assign.title}</h4>
+                                        <h5>{assign.course}</h5>
+                                    </div>
+                                <Link
+                                    key={assign._id}
+                                    to={`/Kanbas/Courses/${courseId}/Assignments/${assign._id}`}
+                                    className="no-underline">
+                                    
+                                    <button
+                                        type="button"
+                                        class="btn btn-warning float-right"
+                                        onClick={() => dispatch(selectAssignment(assign))}>
+                                        Edit
+                                    </button>
+                                </Link>
+
+                                <button
+                                    type="button"
+                                    class="btn btn-danger float-right"
+                                    onClick={() => dispatch(deleteAssignment(assign._id))}>
+                                    Delete
+                                </button>
+
+
+                            </li>
+
+                        ))
+                }
             </div>
         </div >
     );
